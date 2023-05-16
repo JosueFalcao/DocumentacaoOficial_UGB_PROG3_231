8ca48c8013fb8868106be4757681aff9b2584e0b
Ekko <Ekko@EKKO>
2023-05-15 23:21:12 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Data/RelacionamentoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relacionamentos.Models;
+
+namespace Relacionamentos.Data
+{
+    public class RelacionamentoContext: DbContext
+    {
+        public RelacionamentoContext(DbContextOptions<RelacionamentoContext> options): base(options) { }
+        public DbSet<Blog> Blogs  { get; set; } 
+        public DbSet<Post> Posts { get; set; }
+    }
+}
